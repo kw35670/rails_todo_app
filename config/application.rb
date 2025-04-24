9c7bc4d0a48536b98e4279ba72754cdc905ef0c3
r-kawaguchi <R.riverJob1590@gmail.com>
2025-04-24 16:33:38 +0900

    Rspecの初期設定を完了

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,11 @@ module TodoApp
   class Application < Rails::Application
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
-  end
 
-  config.generators do |g|
-    g.assets false
-    g.helper false
-    g.test_framework :rspec
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec
+    end
   end
 end
