8bbd61f42bad81182ecc805daafd263acf0159b0
r-kawaguchi <R.riverJob1590@gmail.com>
2025-04-29 23:00:17 +0900

    ログイン失敗およびログアウト時のメッセージ表示

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,27 @@ Bundler.require(*Rails.groups)
 
 module TodoApp
   class Application < Rails::Application
-    config.load_defaults 8.0
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.load_defaults 7.1
+    config.autoload_lib(ignore: %w(assets tasks))
 
     config.generators do |g|
       g.assets false
       g.helper false
       g.test_framework :rspec
     end
+
+    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'SAMEORIGIN',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff',
+      'X-Download-Options' => 'noopen',
+      'X-Permitted-Cross-Domain-Policies' => 'none',
+      'Referrer-Policy' => 'strict-origin-when-cross-origin'
+    }
+
+    # デフォルトのロケールを日本語に設定
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
