187be9a2e068dac600d99197809d7ddf32b25045
r-kawaguchi <R.riverJob1590@gmail.com>
2025-05-13 00:37:05 +0900

    チーム追加および更新時にエラーメッセージを表示するように修正。

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,8 @@ class Team < ApplicationRecord
   has_many :users, through: :team_users
   has_many :tasks, dependent: :destroy
 
+  validates :name, presence: true, length: { minimum: 1, maximum: 50 }
+
   def display_name
     if name.length > 5
       name[0..4] + "..."
